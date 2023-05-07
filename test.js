64c62991eb9520489fecd171a5a572847d5a5a09
robin0151 <ahammedrobin1952@gmail.com>
2023-05-07 23:06:02 +0600

    test commit

---
+++ test.js
@@ -0,0 +1,52 @@
+// var roastGiven = 0;
+// while (roastGiven < 10) {
+//     console.log('Roast den, Please!', roastGiven);
+//     roastGiven++;
+// }
+
+// var number = 1;
+// while (number <= 20) {
+//     console.log(number);
+//     number++;
+// }
+
+
+// var number = 0;
+// while (number <= 20) {
+//     console.log(number);
+//     number += 2;
+// }
+
+
+// var number = 1;
+
+// while (number <= 20) {
+//     console.log(number);
+//     number += 2;
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// for (let i = 0; i <= 10; i+=2) {
+//     console.log(i);
+// }
+
+// for (let i = 1; i <= 10; i+=2) {
+//     console.log(i);
+// }
+
+var numbers = [45, 87, 89, 56, 32, 51, 25, 188, 41, 25, 98];
+
+for (let i = 0; i < numbers.length; i++) {
+    var number = numbers[i];
+    console.log(number);
+}
+
+var items = ['bottle', 'mouse', 'sunglass', 'pen', 'notebook'];
+
+for (let i = 0; i < items.length; i++) {
+    var item = items[i];
+    console.log(item);
+}
