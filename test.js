eb812e451119a6f58f4dc236d1f7b8662d182108
robin0151 <ahammedrobin1952@gmail.com>
2023-05-08 10:22:04 +0600

    continue and break

--- test.js
+++ test.js
@@ -37,16 +37,41 @@
 //     console.log(i);
 // }
 
-var numbers = [45, 87, 89, 56, 32, 51, 25, 188, 41, 25, 98];
+// var numbers = [45, 87, 89, 56, 32, 51, 25, 188, 41, 25, 98];
 
-for (let i = 0; i < numbers.length; i++) {
-    var number = numbers[i];
-    console.log(number);
-}
+// for (let i = 0; i < numbers.length; i++) {
+//     var number = numbers[i];
+//     console.log(number);
+// }
 
-var items = ['bottle', 'mouse', 'sunglass', 'pen', 'notebook'];
+// var items = ['bottle', 'mouse', 'sunglass', 'pen', 'notebook'];
 
-for (let i = 0; i < items.length; i++) {
-    var item = items[i];
-    console.log(item);
-}
+// for (let i = 0; i < items.length; i++) {
+//     var item = items[i];
+//     console.log(item);
+// }
+
+// for(var i = 1; i <= 20; i++) {
+//     console.log(i);
+//     if (i == 5){
+//         break;
+//     }
+// }
+
+// var items = ['bottle', 'mouse', 'sunglass', 'pen', 'notebook'];
+// for(var i = 0; i < items.length; i++){
+//     var item = items[i];
+//     if (item == 'pen') {
+//         break;
+//     }
+//     console.log(item);
+// }
+
+// var numbers = [45, 101, 87, 89, 56, 102, 32, 51, 25, 188, 41, 25, 98];
+// for (var i = 0; i < numbers.length; i++) {
+//     var number = numbers[i];
+//     if (number > 50) {
+//         continue;   
+//     }
+//     console.log(number);
+// }
